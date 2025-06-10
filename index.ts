b43ee93471ab52fdd674affda31313e4d112970d
prinit1522 <prinittechnologies1522@gmail.com>
2025-06-11 00:08:37 +0530

    added fix for cors origin policy

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 import express from "express"
 import mongoose from "mongoose"
+import cors from "cors";
 const app = express();
-import {RegisterRouter} from "./routes/register.routes";
-const port = 8000;
+app.use(cors({
+  origin: 'http://localhost:5173', // Your frontend origin
+}));
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+import {RegisterRouter} from "./routes/register.routes";
 app.use('/api/register',RegisterRouter)
+const port = 8000;
 async function start() {
     try{
   const connection = await mongoose.connect("mongodb://127.0.0.1:27017/crud");
